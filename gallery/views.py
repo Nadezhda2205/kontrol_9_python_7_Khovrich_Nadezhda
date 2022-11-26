38454f2778a5841f7f5efe42ee1034a744d54c72
Nadezhda Khovrich <hope28281@gmail.com>
2022-11-26 13:03:08 +0600

    Обновлена фикстура

--- gallery/views.py
+++ gallery/views.py
@@ -1,26 +1,23 @@
 from django.shortcuts import render
-from django.views.generic import ListView
-from accounts.models import Account
+from django.views.generic import ListView, DetailView
 
 from gallery.models import Photo
 
 
 class PhotoListView(ListView):
-    template_name: str = 'gallery/list_photo.html'
+    template_name: str = 'gallery/photo_list.html'
     model = Photo
     context_object_name = 'photos'
     
 
+class PhotoDetailView(DetailView):
+    template_name = 'gallery/photo_detail.html'
+    model = Photo
+    context_object_name = 'photos'
+
     # def get_context_data(self, **kwargs):
     #     context = super().get_context_data(**kwargs)
-    #     if not self.request.user.is_authenticated:
-    #         return context
-    #     print(self.request.user.is_authenticated)
-    #     # user: Account = self.request.user
-    #     # subscriptions = user.subscriptions.all()
-    #     photo = Photo.objects.all()
-    #     # subscriptions_posts = posts.filter(author__in = subscriptions)
-    #     context['photo'] = photo
-
+    #     photo_form = CommentForm()
+    #     context['comment_form'] = comment_form
     #     return context
 
