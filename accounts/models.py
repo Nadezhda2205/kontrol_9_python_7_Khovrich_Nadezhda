87449be5d61a5781f4e0fee523fdba5098675aec
Nadezhda Khovrich <hope28281@gmail.com>
2022-11-26 14:17:58 +0600

    Добавление поля в Account

--- accounts/models.py
+++ accounts/models.py
@@ -7,7 +7,12 @@ class Account(AbstractUser):
     name = models.CharField(max_length=150)
     phone = models.CharField(max_length=20, verbose_name='Телефон')
     description = models.TextField(null=True, blank=True)
-
+    favourited_photos = models.ManyToManyField(
+        verbose_name='Избранные фото',
+        to='gallery.Photo',
+        through='gallery.Favourite',
+        related_name='favourited_users'
+        )
     class Meta:
         verbose_name = 'Профиль'
         verbose_name_plural = 'Профили'
